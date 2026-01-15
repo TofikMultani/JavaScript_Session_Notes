1a7dfe3d96a7b312cbf1247c6cf125ccc3506349
TofikMultani <tofikmultani05@gmail.com>
2026-01-15 20:14:08 +0530

    15/1/2026_Task Add

---
+++ Assessment/Assignmet_2/Task1.js
@@ -0,0 +1,15 @@
+let marks =[45,78,98,32,67,88,56];
+
+console.log(marks);
+
+marks.push(100);
+console.log(marks);
+
+marks.unshift(42);
+console.log(marks);
+
+marks.shift();
+console.log(marks);
+
+console.log(marks[3]);
+
